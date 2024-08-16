9c69ee828f6101818364d738a3f8c1c4d58ddc7d
Jonas Karimi <ykarimi987654321@gmail.com>
2024-08-17 00:02:32 +0330

    added playermovements

---
+++ Assets/player2movement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2movement : MonoBehaviour
+{
+  [SerializeField] float speed =0.1f;
+    [SerializeField] int  speed_multiplier=1;
+    [SerializeField] int max_speed =5;
+    private Rigidbody2D pRacket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pRacket = this.GetComponent <Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        Vector2 basespeed = new Vector2 (0f,0f);
+        if(Input.GetKey(KeyCode.W))
+        {
+            if(basespeed.y<max_speed)
+                basespeed+= new Vector2(0f,speed*speed_multiplier);
+            pRacket.velocity = basespeed; 
+            
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            if(basespeed.y>-1*max_speed)
+                basespeed+= new Vector2(0f,-1*speed*speed_multiplier);
+            pRacket.velocity = basespeed;
+        }
+       
+
+    }
+}
